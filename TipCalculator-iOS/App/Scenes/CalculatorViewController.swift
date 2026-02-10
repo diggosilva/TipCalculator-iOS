d4032bc7e4560b51b56ce28fd7dd978f66249c33
Diggo Silva <rdiggosilva@gmail.com>
2026-02-10 12:17:40 -0300

    Initial Configuration

---
+++ TipCalculator-iOS/App/Scenes/CalculatorViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorViewController.swift
+//  TipCalculator-iOS
+//
+//  Created by Diggo Silva on 10/02/26.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Calculadora de Gorjetas"
+    }
+
+}
