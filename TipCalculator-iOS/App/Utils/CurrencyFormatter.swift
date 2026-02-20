7fa020d3a98e1b59d8df696f1aec230458c97245
Diggo Silva <rdiggosilva@gmail.com>
2026-02-20 08:17:35 -0300

    Created CurrencyFormatter file

--- TipCalculator-iOS/App/Utils/CurrencyFormatter.swift
+++ TipCalculator-iOS/App/Utils/CurrencyFormatter.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum CurrencyFormatter {
+    
+    private static let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale.autoupdatingCurrent
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }()
+    
+    static func string(from value: Double) -> String {
+        return formatter.string(from: NSNumber(value: value)) ?? ""
+    }
+    
+    static var zero: String {
+        return string(from: 0)
+    }
+}
