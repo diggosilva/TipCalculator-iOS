3747eef0d303742587deb67e347c52f80f07ffff
Diggo Silva <rdiggosilva@gmail.com>
2026-02-20 08:16:28 -0300

    Xcode configuration

---
+++ TipCalculator-iOS/App/Utils/CurrencyFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  CurrencyFormatter.swift
+//  TipCalculator-iOS
+//
+//  Created by Diggo Silva on 19/02/26.
+//
+
+import Foundation
---
+++ TipCalculator-iOS/App/Utils/String+Localized.swift
@@ -0,0 +1,8 @@
+//
+//  String+Localized.swift
+//  TipCalculator-iOS
+//
+//  Created by Diggo Silva on 19/02/26.
+//
+
+import Foundation
