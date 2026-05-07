245c166bb70337fd66f8f117e7fc1ca6bbe95e9d
Diggo Silva <rdiggosilva@gmail.com>
2026-05-07 10:30:49 -0300

    feat: create DSIdentifiers enum for centralized UI keys

--- TipCalculator-iOS/Shared/DSIdentifiers.swift
+++ TipCalculator-iOS/Shared/DSIdentifiers.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum DSIdentifiers {
+    static let totalPerPersonValueLabel = "totalPerPersonValueLabel"
+    static let totalBillValueLabel = "totalBillValueLabel"
+    static let tipAmountValueLabel = "tipAmountValueLabel"
+    static let billAmountTextField = "billAmountTextField"
+    
+    static let tip10PercentButton = "tip10PercentButton"
+    static let tip15PercentButton = "tip15PercentButton"
+    static let tip20PercentButton = "tip20PercentButton"
+    static let customTipButton = "customTipButton"
+    
+    static let decreasePeopleButton = "decreasePeopleButton"
+    static let peopleCountValueLabel = "peopleCountValueLabel"
+    static let increasePeopleButton = "increasePeopleButton"
+    
+    static let clearButton = "clearButton"
+    static let customTipAlert = "customTipAlert"
+    static let customTipTextField = "customTipTextField"
+}
